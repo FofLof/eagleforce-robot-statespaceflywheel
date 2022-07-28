86e382670736d0af780264c2fa85343f689e24b9
Ethan See <ethan_see@hotmail.com>
2022-07-28 15:07:16 -0700

    Initial commit

---
+++ src/main/java/com/team2073/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,13 @@
+package com.team2073.robot.subsystems;
+
+import com.team2073.common.periodic.AsyncPeriodicRunnable;
+
+public class ShooterSubsystem implements AsyncPeriodicRunnable {
+    public ShooterSubsystem() {
+        autoRegisterWithPeriodicRunner();
+    }
+    @Override
+    public void onPeriodicAsync() {
+
+    }
+}
